18157b6a3348a5e92f6eed77214cbaa987a52aa1
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-03-09 18:46:17 +0530

    Add /todo/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
+var {ObjectID}=require('mongodb');
 var express=require('express');
 var bodyParser=require('body-parser');
-
 
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
-
 
 var app=express();
 
@@ -27,6 +26,21 @@ app.get('/todos',(req,res)=>{
     res.send({todos});
   },(err)=>{
     res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send(`Invalid ObjectID ${id}`);
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send(`Todo not found with id ${id}`);
+    }
+    res.send({todo});
+  },(err)=>{
+    return res.status(400).send();    //not sending err object ontentionaly
   });
 });
 
