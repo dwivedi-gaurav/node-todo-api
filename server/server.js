5648a7ce1b3937a64f17c41725833799bb4e7a2e
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-03-09 15:05:59 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos',(req,res)=>{
   })
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000,()=>{
   console.log('Listening to port 3000');
 });
