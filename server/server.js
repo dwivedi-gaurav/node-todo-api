3325a14407e8e6c11184c495abe0d6b6a2bd70a9
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-03-23 21:30:28 +0530

    Add route PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var {ObjectID}=require('mongodb');
 var express=require('express');
 var bodyParser=require('body-parser');
@@ -59,6 +60,32 @@ app.delete('/todos/:id',(req,res)=>{
     }
     res.send(todo);
   },(err)=>{
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    res.status(400).send(`Invalid objectID ${id}`);
+    return;
+  }
+
+  if(_.isBoolean(body.completed) && body.completed==true){
+    body.completedAt=new Date().getTime();
+  }else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+        return res.status(404).send(`No TODO found with id ${id}`);
+    }
+    res.send({todo});    //ES6
+  }).catch((e)=>{
     res.status(400).send();
   });
 });
