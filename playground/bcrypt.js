98e4cb08b0ae516c44611b74e2b0b3af6cd7c3c6
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-04-03 19:14:48 +0530

    Add POST /users/login route

--- playground/bcrypt.js
+++ playground/bcrypt.js
@@ -2,13 +2,13 @@ const bcrypt=require('bcryptjs');
 
 var pass='abcd123!';
 
-bcrypt.genSalt(10,(err,salt)=>{
-  bcrypt.hash(pass,salt,(err,hash)=>{
-    console.log(hash);
-  });
-});
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(pass,salt,(err,hash)=>{
+//     console.log(hash);
+//   });
+// });
 
-var hashedPass='$2a$10$FVPfkNcpTtHeQaZ2i4VxMeeaU53AmY4biX5W09uNxxsHWvvJI.Mwa';
+var hashedPass='$2a$10$pGI.n7VN2QAl/Zl0hysRUOfoyog0hvT7AccVhJogFXcc9Xjtm5Tr6';
 
 bcrypt.compare(pass,hashedPass,(err,res)=>{
   console.log(res);
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 var {authenticate}=require('./middleware/authenticate');
+var bcrypt=require('bcryptjs');
+
 
 var app=express();
 
@@ -102,10 +104,20 @@ app.post('/users',(req,res)=>{
   });
 });
 
-
-
 app.get('/users/me',authenticate,(req,res)=>{
   res.send(req.user);
+});
+
+app.post('/users/login',(req,res)=>{
+  var email=req.body.email;
+  var password=req.body.password;
+  User.findByCredentials(email,password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+        res.header('x-auth',token).send(user);
+    });
+  }).catch((e)=>{
+    res.status(400).send();
+  });
 });
 
 app.listen(port,()=>{
